ab39d76f8773ed29dea8ab01debe67c7316e9a2a
POTI <potistudio2022@gmail.com>
2024-01-14 00:57:44 +0900

    Basic Chart ScriptableObject

---
+++ Assets/_Rhynux_/Scripts/Chart.cs
@@ -0,0 +1,27 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+using Alchemy.Inspector;
+
+[CreateAssetMenu(fileName = "Chart", menuName = "Rhynux/Chart")]
+public class Chart : ScriptableObject {
+	public string m_Title;
+	public string Artist;
+	public float m_BPM;
+	[LabelText("Offset (ms)")] public float m_Offset;
+
+	public AudioClip m_MusicClip;
+
+	public List<Note> m_Notes;
+}
+
+[System.Serializable]
+public class Note {
+	Note (float _time, float _pos) {
+		Time = _time;
+		Pos = _pos;
+	}
+
+	[field: SerializeField] public float Time { get; set; }
+	[field: SerializeField] public float Pos { get; set; }
+}
