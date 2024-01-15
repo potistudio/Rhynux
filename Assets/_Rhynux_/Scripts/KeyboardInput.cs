c96fcb30d9d522badd20e04e9ba9170b6bbe5ef1
POTI <potistudio2022@gmail.com>
2024-01-15 23:52:43 +0900

    feat: Pressed & Released Event to DFJK Key

--- Assets/_Rhynux_/Scripts/KeyboardInput.cs
+++ Assets/_Rhynux_/Scripts/KeyboardInput.cs
@@ -15,10 +15,26 @@ public class KeyboardInput : IInputInterface {
 	}
 
 	private void Enable() {
+		m_KeyboardActions.Pressed.D.performed += PressedD;
 		m_KeyboardActions.Pressed.F.performed += PressedF;
+		m_KeyboardActions.Pressed.J.performed += PressedJ;
+		m_KeyboardActions.Pressed.K.performed += PressedK;
+
+		m_KeyboardActions.Released.D.performed += ReleasedD;
+		m_KeyboardActions.Released.F.performed += ReleasedF;
+		m_KeyboardActions.Released.J.performed += ReleasedJ;
+		m_KeyboardActions.Released.K.performed += ReleasedK;
 
 		m_KeyboardActions.Enable();
 	}
 
+	private void PressedD (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnPressed.OnNext (0); }
 	private void PressedF (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnPressed.OnNext (1); }
+	private void PressedJ (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnPressed.OnNext (2); }
+	private void PressedK (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnPressed.OnNext (3); }
+
+	private void ReleasedD (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnReleased.OnNext (0); }
+	private void ReleasedF (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnReleased.OnNext (1); }
+	private void ReleasedJ (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnReleased.OnNext (2); }
+	private void ReleasedK (UnityEngine.InputSystem.InputAction.CallbackContext _context) { m_OnReleased.OnNext (3); }
 }
