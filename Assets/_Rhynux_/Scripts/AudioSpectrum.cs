09cc37ce38ad5181efc8bfa983e8d3d39b203125
POTI <potistudio2022@gmail.com>
2024-02-04 15:11:52 +0900

    feat: Draw handles in editor

--- Assets/_Rhynux_/Scripts/AudioSpectrum.cs
+++ Assets/_Rhynux_/Scripts/AudioSpectrum.cs
@@ -1,6 +1,11 @@
 
 using UnityEngine;
 
+#if UNITY_EDITOR
+	using System.Linq;
+	using UnityEditor;
+#endif
+
 public class AudioSpectrum : MonoBehaviour {
 	[SerializeField] private AudioSource m_AudioSource;
 
@@ -10,4 +15,11 @@ public class AudioSpectrum : MonoBehaviour {
 	private void Update() {
 		m_AudioSource.GetOutputData (m_OutputAudioData, 0);
 	}
+
+	#if UNITY_EDITOR
+		private void OnDrawGizmos() {
+			Handles.color = Color.white;
+			Handles.DrawAAPolyLine (2f, OutputAudioData.Select((x, i) => new Vector3(i, x * 32f, 0)).ToArray());
+		}
+	#endif
 }
