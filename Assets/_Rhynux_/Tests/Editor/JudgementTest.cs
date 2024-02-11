62f27d5fb1b23c89f57c06839379ab38e8d4cec5
POTI <potistudio2022@gmail.com>
2024-02-11 22:55:12 +0900

    change: Resolve References

---
+++ Assets/_Rhynux_/Tests/Editor/JudgementTest.cs
@@ -0,0 +1,19 @@
+
+using NUnit.Framework;
+
+public class JudgementTest {
+	private RealtimeReferee m_RealtimeReferee = default;
+
+	[SetUp]
+	public void SetUp() {
+		var a = System.Collections.Generic.List<Note> notes = new() {
+			new Note (0f, 0), new Note (1000f, 0), new Note (2000f, 0), new Note (3000f, 0)
+		};
+		m_RealtimeReferee = new (a);
+	}
+
+	[Test]
+	public void Test() {
+		m_RealtimeReferee.UpdateTime (2000f);
+	}
+}
