e45f01a2f350d9fe3930da0a1f4fa25a9ff324a7
POTI <potistudio2022@gmail.com>
2024-02-21 17:37:00 +0900

    add: Reference of Reactive Referee

--- Assets/_Rhynux_/Scripts/Main/Model/RefereePresenter.cs
+++ Assets/_Rhynux_/Scripts/Main/Model/RefereePresenter.cs
@@ -4,14 +4,17 @@ using UniRx;
 public class RefereePresenter {
 	private readonly SessionManager m_SessionManager;
 	private readonly RealtimeReferee m_RealtimeReferee;
+	private readonly ReactiveReferee m_ReactiveReferee;
 
-	public RefereePresenter (SessionManager _sessionManager, RealtimeReferee _realtimeReferee) {
+	public RefereePresenter (SessionManager _sessionManager, RealtimeReferee _realtimeReferee, ReactiveReferee _reactiveReferee) {
 		m_SessionManager = _sessionManager;
 		m_RealtimeReferee = _realtimeReferee;
+		m_ReactiveReferee = _reactiveReferee;
 
 		// When Session Time is Updated, Update the Realtime Referee
 		m_SessionManager.OnTimeUpdated.Subscribe (x => {
 			m_RealtimeReferee.UpdateTime (x);
+			m_ReactiveReferee.UpdateTime (x);
 		});
 
 		// When Notes Updated by Realtime Referee, also Update the Notes in Session Manager
