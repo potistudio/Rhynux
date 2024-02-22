4c39f90b55cfaa3bf085c05a6a2bf40e0d4fd458
POTI <potistudio2022@gmail.com>
2024-02-23 03:41:27 +0900

    change: Cleanup View Dependece

--- Assets/_Rhynux_/Scripts/Main/View/NotesObjectGenerator.cs
+++ Assets/_Rhynux_/Scripts/Main/View/NotesObjectGenerator.cs
@@ -1,19 +1,15 @@
 
 using UnityEngine;
-using UniRx;
 
 public class NotesObjectGenerator : MonoBehaviour {
 	[SerializeField] private GameObject m_NotePrefab;
 
-	[VContainer.Inject]
-	public void Init (INotesGenerator _notesGenerator) {
-		_notesGenerator.OnNotesGenerated.Subscribe (notes => {
-			Transform notesParent = new GameObject ("Notes Parent").transform;
+	public void GenerateNotes (System.Collections.Generic.List<Note> _notes) {
+		Transform notesParent = new GameObject ("Notes Parent").transform;
 
-			foreach (Note note in notes) {
-				Transform noteObj = Instantiate (m_NotePrefab, new Vector3(note.Position, 0, note.Time), Quaternion.identity).transform;
-				noteObj.SetParent (notesParent);
-			}
-		});
+		foreach (Note note in _notes) {
+			Transform noteObj = Instantiate (m_NotePrefab, new Vector3(note.Position, 0, note.Time), Quaternion.identity).transform;
+			noteObj.SetParent (notesParent);
+		}
 	}
 }
