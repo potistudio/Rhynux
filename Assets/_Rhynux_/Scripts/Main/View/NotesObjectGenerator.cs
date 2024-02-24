dd8e1e36a4f8ae9733160958ec6ebe9b5f48af05
POTI <potistudio2022@gmail.com>
2024-02-24 20:55:36 +0900

    feat: Return List that Contain Generated Notes GameObject

--- Assets/_Rhynux_/Scripts/Main/View/NotesObjectGenerator.cs
+++ Assets/_Rhynux_/Scripts/Main/View/NotesObjectGenerator.cs
@@ -1,15 +1,24 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NotesObjectGenerator : MonoBehaviour {
 	[SerializeField] private GameObject m_NotePrefab;
 
-	public void GenerateNotes (System.Collections.Generic.List<Note> _notes) {
+	private List<GameObject> m_GeneratedNotesObjectList = new();
+	public List<GameObject> GeneratedNotesObjectList => m_GeneratedNotesObjectList;
+
+	public List<GameObject> GenerateNotes (List<Note> _notes) {
 		Transform notesParent = new GameObject ("Notes Parent").transform;
+		m_GeneratedNotesObjectList = new();
 
 		foreach (Note note in _notes) {
 			Transform noteObj = Instantiate (m_NotePrefab, new Vector3(note.Position, 0, note.Time), Quaternion.identity).transform;
 			noteObj.SetParent (notesParent);
+
+			m_GeneratedNotesObjectList.Add (noteObj.gameObject);
 		}
+
+		return m_GeneratedNotesObjectList;
 	}
 }
