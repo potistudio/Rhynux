09e4f80f2cfbbb40aeca7856282de791b194f53b
POTI <potistudio2022@gmail.com>
2024-07-12 00:31:10 +0900

    👍scene entry point

---
+++ Assets/_Rhynux_/Scripts/Title/SceneEntryPoint.cs
@@ -0,0 +1,7 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using MackySoft.Navigathena.SceneManagement;
+
+public sealed class SceneEntryPoint : SceneEntryPointBase {
+}
