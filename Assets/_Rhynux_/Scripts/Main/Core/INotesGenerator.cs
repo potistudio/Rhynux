545adcbf56ce2bfc87bc33606998d368ebcc9c7f
POTI <potistudio2022@gmail.com>
2024-07-13 20:28:36 +0900

    ⚙ fix dependecies

--- Assets/_Rhynux_/Scripts/Main/Core/INotesGenerator.cs
+++ Assets/_Rhynux_/Scripts/Main/Core/INotesGenerator.cs
@@ -1,4 +1,3 @@
-
 public interface INotesGenerator {
 	System.Collections.Generic.List<Note> Generate (Chart _chart);
 	System.IObservable<System.Collections.Generic.IReadOnlyList<Note>> OnNotesGenerated { get; }
