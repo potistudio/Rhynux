7f177c202dfdd960129b4a8ff1d9291117c811f9
POTI <potistudio2022@gmail.com>
2024-07-13 20:22:35 +0900

    👍 basic initializing flow

--- Assets/_Rhynux_/Scripts/Main/SceneEntryPoint.cs
+++ Assets/_Rhynux_/Scripts/Main/SceneEntryPoint.cs
@@ -4,8 +4,22 @@ using Cysharp.Threading.Tasks;
 using MackySoft.Navigathena.SceneManagement;
 
 public sealed class SceneEntryPoint : SceneEntryPointBase {
+	[SerializeField] private MusicPlayer m_MusicPlayer;
+	[SerializeField] private Chart m_Chart;
+
+	protected override UniTask OnInitialize(ISceneDataReader reader, System.IProgress<MackySoft.Navigathena.IProgressDataStore> progress, CancellationToken cancellationToken) {
+		Debug.Log ("Initialize");
+
+		m_MusicPlayer.Clip = m_Chart.Clip;
+
+		return base.OnInitialize (reader, progress, cancellationToken);
+	}
+
 	protected override UniTask OnEnter (ISceneDataReader reader, CancellationToken cancellationToken) {
 		Debug.Log ("Enter");
+
+		m_MusicPlayer.Play();
+
 		return base.OnEnter (reader, cancellationToken);
 	}
 }
