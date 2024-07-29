85a2ce8f72cb217edb92f3aa366ca50b63b22ceb
POTI <potistudio2022@gmail.com>
2024-07-30 01:30:22 +0900

    🔧 fix auto generation of scroller

--- Assets/_Rhynux_/Scripts/SongSelection/View/ScrollItemRegistrar.cs
+++ Assets/_Rhynux_/Scripts/SongSelection/View/ScrollItemRegistrar.cs
@@ -12,9 +12,27 @@ public class ScrollItemRegistrar : MonoBehaviour {
 
 	[VContainer.Inject]
     private void Init (ScrollView _scrollView) {
-        if (m_AutoGeneration)
-            Enumerable.Range (0, m_GenerationCount);
+		IEnumerable<Chart> generatedCharts;
 
-        _scrollView.UpdateData (m_Charts.Select(x => x.Chart).ToArray());
+        if (m_AutoGeneration) {
+            var i = Enumerable.Range (0, m_GenerationCount);
+			generatedCharts = i.Select (_ => new Chart(RandomBase64(), RandomBase64(), 120f, 0f, null, new Note[0]));
+		} else {
+			generatedCharts = m_Charts.Select(x => x.Chart);
+		}
+
+        _scrollView.UpdateData (generatedCharts.ToArray());
     }
+
+	private string RandomBase64() {
+		// Generate GUID as a Base64 string
+		System.Byte[] guidBytes = System.Guid.NewGuid().ToByteArray();
+		string base64 = System.Convert.ToBase64String (guidBytes);
+
+		// Remove last 3 characters '(QAgw)=='
+		System.Text.RegularExpressions.Regex regex = new (".{3}$");
+		string result = regex.Replace (base64, " ");
+
+		return result;
+	}
 }
