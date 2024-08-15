aa948ef972430b3c193345cb9c9884054db50b89
POTI <potistudio2022@gmail.com>
2024-08-15 11:30:03 +0900

    ✨fix scene navigation

---
+++ Assets/_Rhynux_/Scripts/SongSelection/Input/MenuInput.cs
@@ -0,0 +1,44 @@
+using MackySoft.Navigathena.SceneManagement;
+
+public sealed class MenuInput : UnityEngine.MonoBehaviour {
+	private ISceneIdentifier m_SceneIdentifier;
+	private Selection m_SelectionAction;
+
+	private void Awake() {
+		m_SceneIdentifier = new BuiltInSceneIdentifier ("Sample");
+		m_SelectionAction = new Selection();
+
+		m_SelectionAction.Menu.MenuNext.performed += NextMenu;
+		m_SelectionAction.Menu.MenuBack.performed += BackMenu;
+
+		m_SelectionAction.Enable();
+	}
+
+	private void OnEnable() {
+		m_SelectionAction?.Enable();
+	}
+
+	private void OnDisable() {
+		m_SelectionAction?.Disable();
+	}
+
+	private void OnDestroy() {
+		m_SelectionAction.Dispose();
+	}
+
+	private void BackMenu (UnityEngine.InputSystem.InputAction.CallbackContext _context) {
+		BackScene();
+	}
+
+	private void NextMenu (UnityEngine.InputSystem.InputAction.CallbackContext _context) {
+		NextScene();
+	}
+
+	private async void NextScene() {
+		await GlobalSceneNavigator.Instance.Push (m_SceneIdentifier);
+	}
+
+	private async void BackScene() {
+		await GlobalSceneNavigator.Instance.Pop ();
+	}
+}
