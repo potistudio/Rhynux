c4022adab4ec28f569d99cf79fae3c1d0b586924
POTI <potistudio2022@gmail.com>
2024-09-27 05:28:21 +0900

    ✨ add getter setter

--- Assets/_Rhynux_/Scripts/Game/Proxy/SessionProxy.cs
+++ Assets/_Rhynux_/Scripts/Game/Proxy/SessionProxy.cs
@@ -1,7 +1,5 @@
 public sealed class SessionProxy {
 	private SessionData m_SessionData;
 
-	public void SetSessionData (SessionData _data) {
-		m_SessionData = _data;
-	}
+	public SessionData Session { get; set; }
 }
