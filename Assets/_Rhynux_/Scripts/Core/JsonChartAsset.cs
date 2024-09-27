b170b08d27605375a19c4842ae78dbd085806b61
POTI <potistudio2022@gmail.com>
2024-09-28 06:11:08 +0900

    👍 add composer field to json chart

--- Assets/_Rhynux_/Scripts/Core/JsonChartAsset.cs
+++ Assets/_Rhynux_/Scripts/Core/JsonChartAsset.cs
@@ -19,7 +19,7 @@ public sealed class JsonChartAsset : ChartAsset {
 				return new Note (time, lane);
 			}).ToArray();
 
-			return new Chart (jsonChart.name, "Unknown", jsonChart.BPM, jsonChart.offset * 0.0001f, new SoundTrack(m_SongClip), notes, m_Artwork, m_Secured);
+			return new Chart (jsonChart.name, m_Composer, jsonChart.BPM, jsonChart.offset * 0.0001f, new SoundTrack(m_SongClip), notes, m_Artwork, m_Secured);
 		}
 	}
 
